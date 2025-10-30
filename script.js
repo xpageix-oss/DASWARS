f31c7b61e2760a86048cf7b14e820ceabc554b2f
xpageix-oss <xpageix@gmail.com>
2025-10-30 15:23:10 +0100

    script.js aktualisieren

--- script.js
+++ script.js
@@ -122,7 +122,7 @@ invitationButton.addEventListener('click', () => {
             pascalImage.classList.remove("hidden");
             pascalImage.classList.add("visible");
           }
-        }, 90000);
+        }, 82000);
 
       }, 12000);
     }, 3000);
